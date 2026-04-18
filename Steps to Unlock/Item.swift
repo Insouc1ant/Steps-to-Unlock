7ce9502969206227eb35a3370f6a3a6f786d7a5b
Insouc1ant <ferdynand.kee09@gmail.com>
2026-04-18 11:48:24 +0700

    Initial Commit

---
+++ Steps to Unlock/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Steps to Unlock
+//
+//  Created by Ferdynand Kee on 18/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
